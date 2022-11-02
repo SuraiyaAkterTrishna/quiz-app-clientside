63f4f5b90928cb9d78516bb3b90551aa11931585
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-02 22:24:54 +0600

    daisy ui successful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <button className='btn btn-success'>Hello Quiz</button>
     </div>
   );
 }
